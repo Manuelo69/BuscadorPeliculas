268bb5b483d08eda9faa93d249f18c75a58ce5aa
manuelo69 <ichigo2418@gmail.com>
2024-04-25 11:23:06 +0200

    arreglos de errores de busqueda del input

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,52 @@
 import { useRef, useState } from "react";
 import "./App.css";
+import ListaPeliculas from "./Components/ListaPeliculas/ListaPeliculas";
 
 function App() {
-  //definir las peliculas
-  const [peliculas, setPeliculas] = useState("");
-  //guardar la ultima busqueda
-  const ultimaBusqueda = useRef(null);
-  //Obtener peliculas de la api
+  const [busqueda, setBusqueda] = useState(""); // Estado para la búsqueda
+  const [peliculas, setPeliculas] = useState([]); // Estado para las películas
+
+  const ultimaBusqueda = useRef(""); // Referencia para la última búsqueda
+
   const getPeliculas = async (peli) => {
-    setPeliculas(peli);
     const response = await fetch(
-      `https://omdbapi.com/?s=${peli}*&apikey=dd8f4ac5`
+      `https://omdbapi.com/?s=${peli}&apikey=dd8f4ac5`
     );
     const data = await response.json();
-    return data;
+    setPeliculas(data.Search || []); // Actualizar el estado de las películas
   };
-  //Controlar el input
+
   const handleInputChange = (event) => {
-    setPeliculas(event.target.value);
+    setBusqueda(event.target.value); // Actualizar el estado de la búsqueda
   };
-  //controlar el boton
+
   const handleButtonClick = () => {
-    if (peliculas.trim() !== ultimaBusqueda.current) {
-      ultimaBusqueda.current = peliculas.trim();
-      getPeliculas(ultimaBusqueda.current);
+    if (busqueda.trim() !== ultimaBusqueda.current.trim()) {
+      getPeliculas(busqueda.trim());
+      ultimaBusqueda.current = busqueda.trim(); // Actualizar la última búsqueda
     }
   };
+
   return (
     <>
       <form action="#">
-        <h1>Buscador de peliculas</h1>
+        <h1>Buscador de películas</h1>
         <label htmlFor="peliculas">
-          <legend>Introduce pelicula</legend>
+          <legend>Introduce película</legend>
           <input
             type="text"
             name="peliculas"
             id="peliculas"
-            placeholder="Introduce pelicula"
-            value={peliculas}
+            placeholder="Introduce película"
+            value={busqueda} // Usar el valor del estado de búsqueda
             onChange={handleInputChange}
           />
         </label>
-        <button onClick={handleButtonClick}>Buscar Pelicula</button>
+        <button type="button" onClick={handleButtonClick}>
+          Buscar Película
+        </button>
       </form>
-      <table></table>
+      <ListaPeliculas peliculas={peliculas} />
     </>
   );
 }
