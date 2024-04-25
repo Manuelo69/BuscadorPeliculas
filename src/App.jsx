109cfd3dd0904ff446b0318d537c283f8cc9951b
manuelo69 <ichigo2418@gmail.com>
2024-04-25 10:03:31 +0200

    boton aplicado con una sola peticion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [peliculas, SetPeliculas] = useState("");
+  const [peliculas, setPeliculas] = useState("");
+
+  const ultimaBusqueda = useRef(null);
+
   const getPeliculas = async (peli) => {
-    SetPeliculas(peli);
+    setPeliculas(peli);
     const response = await fetch(
       `https://omdbapi.com/?s=${peliculas}*&apikey=dd8f4ac5`
     );
@@ -12,12 +15,23 @@ function App() {
     return data;
   };
 
+  const handleInputChange = (event) => {
+    setPeliculas(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    if (peliculas.trim() !== ultimaBusqueda.current) {
+      ultimaBusqueda.current = peliculas.trim();
+      getPeliculas(ultimaBusqueda.current);
+    }
+  };
+
   useEffect(() => {
     getPeliculas();
   }, [peliculas]);
   return (
     <>
-      <form action="">
+      <form action="#">
         <h1>Buscador de peliculas</h1>
         <label htmlFor="peliculas">
           <legend>Introduce pelicula</legend>
@@ -26,8 +40,11 @@ function App() {
             name="peliculas"
             id="peliculas"
             placeholder="Introduce pelicula"
+            value={peliculas}
+            onChange={handleInputChange}
           />
         </label>
+        <button onClick={handleButtonClick}>Buscar Pelicula</button>
       </form>
       <table></table>
     </>
