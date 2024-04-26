ed203cafc7f5bf6207ca5b7e9148f508d7e34128
manuelo69 <ichigo2418@gmail.com>
2024-04-26 11:42:37 +0200

    alineado y grid responsivo hecho

--- src/Components/ListaPeliculas/ListaPeliculas.jsx
+++ src/Components/ListaPeliculas/ListaPeliculas.jsx
@@ -4,13 +4,16 @@ import React from "react";
 
 function ListaPeliculas({ peliculas }) {
   return (
-    <div className="peliculas content-center">
+    <div className="pelicula grid grid-cols-auto-fit-minmax gap-4 content-center">
       {peliculas.map((pelicula) => (
         <div
           key={pelicula.imbdID}
           className="grid grid-flow-col justify-center"
         >
-          <div className="info-pelicula " key={pelicula.imbdID + "2"}>
+          <div
+            className="info-pelicula content-center"
+            key={pelicula.imbdID + "2"}
+          >
             <h3>{pelicula.Title}</h3>
             <p className="font-bold">Año: {pelicula.Year}</p>
             <img src={pelicula.Poster} alt={pelicula.Title} />
